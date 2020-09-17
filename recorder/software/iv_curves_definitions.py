0819546f9a9db5f4cb83a8cdf8312eb9e6731729
Sepehr Mosavat <sayedsepehr.mosavat@uni-due.de>
2020-09-17 18:29:29 +0200

    Add class for describing the energy harvesting conditions

--- recorder/software/iv_curves_definitions.py
+++ recorder/software/iv_curves_definitions.py
@@ -18,3 +18,14 @@ class CurvePoint:
         self.sequence_number = _sequence_number
         self.x = _x
         self.y = _y
+
+
+class HarvestingCondition:
+    def __init__(self, _indoor_or_outdoor: str, _light_intensity: str, _weather_condition: str, _country: str, _city: str):
+        self.indoor_or_outdoor = _indoor_or_outdoor
+        self.light_intensity = _light_intensity
+        self.weather_condition = _weather_condition
+        self.country = _country
+        self.city = _city
+
+
