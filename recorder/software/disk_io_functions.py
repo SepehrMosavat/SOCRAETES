42c77ae2ea88773dc780c42943fa48aa1acd01fe
Sepehr Mosavat <sayedsepehr.mosavat@uni-due.de>
2020-09-17 18:30:11 +0200

    Add metadata about the harvesting conditions

--- recorder/software/disk_io_functions.py
+++ recorder/software/disk_io_functions.py
@@ -3,6 +3,10 @@ import numpy as np
 import queue
 import threading
 
+from iv_curves_definitions import HarvestingCondition
+
+harvesting_condition = HarvestingCondition('indoor', "8", 'sunny', 'germany', 'essen')
+
 
 def write_iv_curves_to_disk(_iv_curves_queue: queue.Queue, _stop_thread_event: threading.Event):
     curve_counter = 0
@@ -11,6 +15,18 @@ def write_iv_curves_to_disk(_iv_curves_queue: queue.Queue, _stop_thread_event: t
     while True:
         if _stop_thread_event.isSet():
             print("Committing curve data to the hard disk...")
+            with h5py.File('captured_traces\\output.hdf5', 'a') as f:
+                harvesting_condition_list = [(np.string_('Indoor/Outdoor'),
+                                             np.string_('Light Intensity (out of 10)'),
+                                             np.string_('Weather Condition'),
+                                             np.string_('Country'),
+                                             np.string_('City')),
+                                             (np.string_(harvesting_condition.indoor_or_outdoor),
+                                             np.string_(harvesting_condition.light_intensity),
+                                             np.string_(harvesting_condition.weather_condition),
+                                              np.string_(harvesting_condition.country),
+                                            np.string_(harvesting_condition.city))]
+                dataset = f.create_dataset('harvesting conditions', data=harvesting_condition_list)
             for arr in data_array_buffer:
                 with h5py.File('captured_traces\\output.hdf5', 'a') as f:
                     dataset = f.create_dataset('curve' + str(curve_counter), data=arr, dtype='f')
