6c2b5c8fffade57852f56f3324ac0a42e386c7fc
Sepehr Mosavat <sayedsepehr.mosavat@uni-due.de>
2021-03-08 18:08:22 +0100

    Use maximum ADC sampling and conversion speed

--- firmware/recorder/Functions.cpp
+++ firmware/recorder/Functions.cpp
@@ -86,14 +86,14 @@ void initializeADC()
 {
 	adc->adc0->setAveraging(2); // set number of averages
 	adc->adc0->setResolution(16); // set bits of resolution
-	adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED); // change the conversion speed
-	adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED); // change the sampling speed
+	adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_HIGH_SPEED); // change the conversion speed
+	adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED); // change the sampling speed
 	adc->adc0->setReference(ADC_REFERENCE::REF_3V3);
 
 	adc->adc1->setAveraging(1); // set number of averages
 	adc->adc1->setResolution(16); // set bits of resolution
 	adc->adc1->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_HIGH_SPEED); // change the conversion speed
-	adc->adc1->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED); // change the sampling speed
+	adc->adc1->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED); // change the sampling speed
 	adc->adc1->setReference(ADC_REFERENCE::REF_3V3);
 }
 
