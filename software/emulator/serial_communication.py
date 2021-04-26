c6942933fc2e345b2034cba2f79ef126a880581f
Sepehr Mosavat <sayedsepehr.mosavat@uni-due.de>
2021-04-27 00:00:16 +0200

    Improve the format and readability of the information printed during emulation

--- software/emulator/serial_communication.py
+++ software/emulator/serial_communication.py
@@ -30,6 +30,8 @@ def serial_port_handling(_trace_emulation_queue: queue.Queue, _stop_thread_event
         else:
             print('Serial port could not be opened')
 
+    print('Emulation Started...')
+    curve_counter = 0
     while True:
         if not _trace_emulation_queue.empty():
             curve_emulation_parameters = _trace_emulation_queue.get()
@@ -44,12 +46,16 @@ def serial_port_handling(_trace_emulation_queue: queue.Queue, _stop_thread_event
 
             if DEBUG_MODE is DebugMode.NO_DEBUG or DEBUG_MODE is DebugMode.DEBUG_WITH_HARDWARE:
                 ser.write(outgoing_serial_byte_array)
+                print(f'Curve {curve_counter:5}' +
+                      f':   OC voltage: {int(curve_emulation_parameters.open_circuit_voltage):7}' +
+                      f' uV, SC current: {int(curve_emulation_parameters.short_circuit_current):5}' +
+                      ' uA')
                 if DEBUG_MODE is DebugMode.DEBUG_WITH_HARDWARE:
                     serial_bytes_received = ser.readline()
                     print(serial_bytes_received.decode('UTF-8'))
             elif DEBUG_MODE is DebugMode.DEBUG_WITHOUT_HARDWARE:
-                print('OC voltage: ' +
-                      str(int(curve_emulation_parameters.open_circuit_voltage)) +
-                      ' uV, SC current: ' +
-                      str(int(curve_emulation_parameters.short_circuit_current)) +
+                print(f'Curve {curve_counter:5}' +
+                      f':   OC voltage: {int(curve_emulation_parameters.open_circuit_voltage):7}' +
+                      f' uV, SC current: {int(curve_emulation_parameters.short_circuit_current):5}' +
                       ' uA')
+            curve_counter = curve_counter + 1
